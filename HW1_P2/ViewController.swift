624fb3ee0037ebb972b694465f679f75309e2bc6
Aleksandr Babkin <plizkinzmey@icloud.com>
2022-06-06 12:48:27 +0300

    Initial Commit

---
+++ HW1_P2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW1_P2
+//
+//  Created by Aleksandr Babkin on 06.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
